3f08714d340f0d0b023523a776653be84a7186a8
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-06 00:24:59 +0000

    Upgrading to 4.1.1: Added quote table, redesigned setting menus, files, and tables
    git-svn-id: https://plugins.svn.wordpress.org/quote-master/trunk@160792 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ quote_master_get_quotes.php
@@ -0,0 +1,46 @@
+<?php
+/*
+This function retrieves the quote from the table
+*/
+function get_quotes() 
+{
+	$quotes_cat = get_option('quote_category');
+	global $wpdb;
+	$sql = "SELECT quote_id, quote, author, cate
+		FROM " . $wpdb->prefix . "quotesmaster";
+	if ($quotes_cat != 0)
+		$sql .= " WHERE cate=" . $quotes_cat;
+	$sql .= " ORDER BY RAND() LIMIT 1";
+	$random_quote = $wpdb->get_row($sql, ARRAY_A);
+
+
+
+	if ( get_option('change_on_load'))
+	{
+		if ( !empty($random_quote) )
+		{
+			return $random_quote["quote"];
+		}
+		else
+			return 0;
+	}
+	elseif ( get_option('last_time') < time() - (get_option('quote_master_reload_time') * 60))
+	{
+		$numbers = time();
+		update_option('last_time' , $numbers);
+		if ( !empty($random_quote) ) 
+		{
+			$data = $random_quote["quote"];
+		}
+		else
+			$data = 0;
+		update_option('current_quote',$data);
+		return $data;
+	}
+	else
+	{
+		$data = get_option('current_quote');
+		return $data;
+	}
+}
+?>
