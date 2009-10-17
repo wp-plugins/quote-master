4904c06d77af3d787cfe1e44034272a02a2f1cd6
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-17 16:05:26 +0000

    Updating to 4.3.1
    git-svn-id: https://plugins.svn.wordpress.org/quote-master/trunk@164345 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ quote_master_support.php
@@ -0,0 +1,15 @@
+<?php
+/*
+This function generates the quotes page
+*/
+function generate_quotes_master_quote_page()
+{
+?>
+	<div class="wrap">
+	<h2>Quote Master Quote List</h2>
+	<p>This will be where the list of quotes will be listed.  Look for this to change by version 5.1.1.
+	This will enable you to edit, add, and/or delete quotes.</p>
+	</div>
+<?php
+}
+?>
