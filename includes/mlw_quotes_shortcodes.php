7f5ae636b6ec6a99bacbe87bb7aa099a7aefdc51
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 17:49:57 +0000

    Adding includes folder to 6.2.3 folder
    git-svn-id: https://plugins.svn.wordpress.org/quote-master/tags/6.2.3@874215 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/mlw_quotes_shortcodes.php
@@ -0,0 +1,32 @@
+<?php
+/*
+This function displays the quote to the user when using the shortcode.
+*/
+function mlw_quotes_shortcode($atts)
+{
+	extract(shortcode_atts(array(
+		'cate' => 'all'
+	), $atts));
+	
+	$mlw_quotes_cate = $cate;
+	if ($mlw_quotes_cate == "all")
+	{
+		$mlw_quotes_cate_sql = "";	
+	}
+	else
+	{
+		$mlw_quotes_cate_sql = " AND category_id=".$mlw_quotes_cate;
+	}
+	$mlw_quotes_display = "";
+	global $wpdb;
+	$mlw_quotes_table_name = $wpdb->prefix . "mlw_quotes";
+	$mlw_quotes_data = $wpdb->get_results( "SELECT * FROM " .$mlw_quotes_table_name. " WHERE deleted='0'".$mlw_quotes_cate_sql." ORDER BY RAND() LIMIT 1" );
+	foreach($mlw_quotes_data as $mlw_quotes_each) {
+		$mlw_quotes_data = $mlw_quotes_each;
+		break;
+	}
+	$mlw_quotes_display .= "<p>'".$mlw_quotes_data->quote."'</p>";
+	$mlw_quotes_display .= "<p>~".$mlw_quotes_data->author."</p>";
+	return $mlw_quotes_display;
+}
+?>
