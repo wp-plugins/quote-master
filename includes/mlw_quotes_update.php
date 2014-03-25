685bef00a5a162f8a1046fd3f62ac4bec8c54f9e
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-25 18:22:01 +0000

    Adding new 6.2.4 tag folder
    git-svn-id: https://plugins.svn.wordpress.org/quote-master/tags/6.2.4@881893 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/mlw_quotes_update.php
@@ -0,0 +1,62 @@
+<?php
+/*
+This is the update function for the plugin. When the plugin gets updated, the database changes are done here. This function is placed in the init of wordpress.
+*/
+function mlw_quotes_update()
+{
+	
+	//Update this variable each update. This is what is checked when the plugin is deciding to run the upgrade script or not.
+	$data = "6.2.4";
+	if ( ! get_option('mlw_quotes_version'))
+	{
+		add_option('mlw_quotes_version' , $data);
+	}
+	elseif (get_option('mlw_quotes_version') != $data)
+	{
+		global $wpdb;
+		$table_name = $wpdb->prefix . "mlw_quotes_cate";
+
+		if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) 
+	
+		{
+	
+			$sql = "CREATE TABLE " . $table_name . " (
+	
+				category_id mediumint(9) NOT NULL AUTO_INCREMENT,
+				
+				category TEXT NOT NULL,
+				
+				deleted INT NOT NULL,
+	
+				PRIMARY KEY  (category_id)
+	
+			);";
+	
+			$results = $wpdb->query( $sql );
+	
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+			dbDelta($sql);
+			
+			$insert = "INSERT INTO " . $table_name .
+			"(category, deleted) " .
+	           	"VALUES ('Ability', 0)";
+			$results = $wpdb->query( $insert );
+			
+			$insert = "INSERT INTO " . $table_name .
+			"(category, deleted) " .
+	           	"VALUES ('Action', 0)";
+			$results = $wpdb->query( $insert );
+			
+			$insert = "INSERT INTO " . $table_name .
+			"(category, deleted) " .
+	           	"VALUES ('Argument', 0)";
+			$results = $wpdb->query( $insert );
+		}
+		update_option('mlw_quotes_version' , $data);
+	}
+	if ( ! get_option('mlw_advert_shows'))
+	{
+		add_option('mlw_advert_shows' , 'true');
+	}
+}
+?>
---
+++ mlw_quoteMaster.php
@@ -0,0 +1,56 @@
+<?php
+
+
+/*
+Plugin Name: Quote Master
+Description: Use this plugin to add quotes to your website.
+Version: 6.2.4
+Author: Frank Corso
+Author URI: http://www.mylocalwebstop.com/
+Plugin URI: http://www.mylocalwebstop.com/
+*/
+
+/* 
+Copyright 2014, My Local Webstop (email : fpcorso@mylocalwebstop.com)
+
+Disclaimer of Warranties. 
+
+The plugin is provided "as is". My Local Webstop and its suppliers and licensors hereby disclaim all warranties of any kind, 
+express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement. 
+Neither My Local Webstop nor its suppliers and licensors, makes any warranty that the plugin will be error free or that access thereto will be continuous or uninterrupted.
+You understand that you install, operate, and unistall the plugin at your own discretion and risk.
+*/
+
+///Files to Include
+include("includes/mlw_quotes_admin.php");
+include("includes/mlw_quotes_category.php");
+include("includes/mlw_quotes_install.php");
+include("includes/mlw_quotes_update.php");
+include("includes/mlw_quotes_shortcodes.php");
+include("includes/mlw_quotes_widgets.php");
+include("includes/mlw_quotes_help.php");
+
+///Activation Actions
+register_activation_hook( __FILE__, 'mlw_quotes_activate');
+add_action('admin_menu', 'mlw_quotes_add_menu');
+add_action('init', 'mlw_quotes_update');
+add_action('widgets_init', create_function('', 'return register_widget("Mlw_Quotes_Quote_Widget");'));
+add_shortcode('mlw_quotes', 'mlw_quotes_shortcode');
+
+//Setup Translations
+function mlw_quotes_translation_setup() {
+  load_plugin_textdomain( 'mlw_quotes_text_domain', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+}
+add_action('plugins_loaded', 'mlw_quotes_translation_setup');
+
+///Create Admin Pages
+function mlw_quotes_add_menu()
+{
+	if (function_exists('add_menu_page'))
+	{
+		add_menu_page('Quote Master', 'Quote Master', 'moderate_comments', __FILE__, 'mlw_quotes_generate_admin');
+		add_submenu_page(__FILE__, 'Categories', 'Categories', 'moderate_comments', 'mlw_quotes_category', 'mlw_quotes_generate_category_page');
+		add_submenu_page(__FILE__, 'Help', 'Help', 'moderate_comments', 'mlw_quotes_help', 'mlw_quotes_generate_help_page');
+	}
+}
+?>
